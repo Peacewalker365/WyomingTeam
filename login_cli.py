d6cec57464a5d6d4837996f2da4a7b73382b3707
Wenchao Li <47430870+Peacewalker365@users.noreply.github.com>
2021-09-19 23:10:46 -0400

    2nd ver.
    Debugged version.
    Working on pytest cases.

--- login_cli.py
+++ login_cli.py
@@ -1,5 +1,41 @@
 import fire
 import welcome
+import string
+
+# Test if the username is valid.
+# Added by Wenchao 9/19/2021 23:00
+def ifNameValid(username):
+    users = getUsers()
+    # First, we will check if the username is unique
+    for user in users:
+        _username = user[0]
+        if _username == username:
+            return False
+    return True
+
+# Test if the Password is valid.
+# Added by Wenchao 9/19/2021 23:00
+def ifPasswordValid(Password):
+    charList = list(Password)
+    capNum = 0
+    length = len(Password)
+    digitNum = 0
+    sepecialNum = 0
+    for char in charList:
+        if char in string.digits:
+            digitNum += 1
+        elif char in string.ascii_uppercase:
+            capNum += 1
+        elif char in string.punctuation:
+            sepecialNum += 1
+        else:
+            pass
+    if length <= 12 and length >= 8 and capNum >=1 and digitNum >=1 and sepecialNum >=1:
+        return True
+    else:
+        return False
+    
+
 
 # We want to get all of the current users and then see if the given username and password match
 def login(username, password):
